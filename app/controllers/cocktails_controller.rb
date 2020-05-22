b4821e366ef40b668485e57583472e41e0f6a089
Sanja Wetzel <sanjwich@gmail.com>
2020-05-22 13:19:12 +0200

    shined it all up

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -14,8 +14,11 @@ class CocktailsController < ApplicationController
 
   def create
     @cocktail = Cocktail.new(cocktail_params)
-    @cocktail.save
-    redirect_to cocktails_path(@cocktail)
+    if @cocktail.save
+      redirect_to @cocktail
+    else
+      render :new
+    end
   end
 
   private
